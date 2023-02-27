171f3b200ad6572f2330eb1c95132ceb150e679c
mohamed67210 <mohamed_bechri@outlook.fr>
2023-02-27 11:36:42 +0100

    faire une requete pour recuperer les users par leurs role dans user repository

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/Coachs', name: 'show_users')]
+    public function showCoachs(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $coachs = $userRepository->findByRole('ROLE_COACH');
+
+        return $this->render('user/showCoachs.html.twig', [
+            'coachs' => $coachs,
         ]);
     }
 }
