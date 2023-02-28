41d981714ce27259d8096304a9a9961b376fbce7
mohamed67210 <mohamed_bechri@outlook.fr>
2023-02-28 16:47:11 +0100

    modif scrollreveal link

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,4 +4,4 @@ console.log('hello')
 window.sr = ScrollReveal({ reset: true });
 
 // Customizing a reveal set
-sr.reveal('.categorie_cart', {interval: 600});
+sr.reveal('.categorie_cart', { interval: 200, scale: 0.2 });
