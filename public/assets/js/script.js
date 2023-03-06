2efd590a24b5e37bfaa9297fc431b6f96daaf61e
mohamed67210 <mohamed_bechri@outlook.fr>
2023-03-06 12:32:10 +0100

    ajouter menu burger dans la nav

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,3 +6,17 @@ window.sr = ScrollReveal({ reset: true });
 // Customizing a reveal set
 sr.reveal('.categorie_cart', { interval: 200, scale: 0.2, reset: true });
 sr.reveal('.about_categorie', { interval: 600, scale: 0.2 });
+
+// menu burger
+var navLinks = document.getElementById("nav-links");
+var btnMenu = document.getElementById("hamburger");
+var body = document.body
+// for (var i = 0; i < btnMenu.length; i++) {
+
+    
+    btnMenu.addEventListener('click', function () {
+        console.log(btnMenu)
+        btnMenu.classList.toggle('is-active');
+        navLinks.classList.toggle('open')
+    });
+// 
