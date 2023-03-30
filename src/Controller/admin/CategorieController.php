e9a6eb4e0137115eb56a97866fd08babf72f0e12
mohamed67210 <mohamed_bechri@outlook.fr>
2023-03-30 13:39:50 +0200

    panel admin : ajout de categorie creation de formulaire

--- src/Controller/admin/CategorieController.php
+++ src/Controller/admin/CategorieController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller\admin;
 
+use App\Entity\Categorie;
+use App\Form\CategorieType;
 use App\Repository\CategorieRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/categories', name: 'admin_categorie_')]
 class CategorieController extends AbstractController
@@ -16,6 +22,47 @@ class CategorieController extends AbstractController
         $categories = $categorieRepository->findBy([], ['id' => 'asc']);
         return $this->render('admin/categories/index.html.twig', [
             'categories' => $categories,
+        ]);
+    }
+    #[Route('/add', name: 'add')]
+    public function addCategorie(ManagerRegistry $doctri,Request $request,Categorie $categorie = null,SluggerInterface $slugger): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $categorie = new Categorie;
+        $form = $this->createForm(CategorieType::class,$categorie);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // upload image
+            $uploadedFile = $form['image']->getData();
+            // dd($uploadedFile);
+            if ($uploadedFile) {
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
+                // dd($newFilename);
+
+                // Move the file to the directory where Programme images are stored
+                try {
+                    $uploadedFile->move(
+                        $this->getParameter('categorieImage_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $categorie->setImage(
+                    $newFilename
+                );
+            }
+        }
+        return $this->render('admin/categories/index.html.twig', [
+
+            'CategorieForm' => $form->createView(),
         ]);
     }
 }
