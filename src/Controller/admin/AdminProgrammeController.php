9483b899f282416f6f42c9839a40304ea2d84807
mohamed67210 <mohamed_bechri@outlook.fr>
2023-03-30 15:12:11 +0200

    activer/desactiver un programme avec affichage de message grace a addflash

--- src/Controller/admin/AdminProgrammeController.php
+++ src/Controller/admin/AdminProgrammeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\admin;
 
+use App\Entity\Programme;
 use App\Repository\ProgrammeRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +16,34 @@ class AdminProgrammeController extends AbstractController
     public function index(ProgrammeRepository $programmeRepository): Response
     {
         $programmes = $programmeRepository->findBy([], ['id' => 'asc']);
+        return $this->render('admin/programmes/index.html.twig', [
+            'programmes' => $programmes,
+        ]);
+    }
+
+    #[Route('/active/{id}', name: 'active')]
+    public function activeProgramme(ManagerRegistry $doctrine, Programme $programme, ProgrammeRepository $programmeRepository): Response
+    {
+        $programme->setisValid(true);
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($programme);
+        $entityManager->flush();
+        $programmes = $programmeRepository->findBy([], ['id' => 'asc']);
+        $this->addFlash("success", "vous venez d'activé le programme : " . $programme->getIntitule() . " !");
+        return $this->render('admin/programmes/index.html.twig', [
+            'programmes' => $programmes,
+        ]);
+    }
+    #[Route('/desactive/{id}', name: 'desactive')]
+    public function desactiveProgramme(ManagerRegistry $doctrine, Programme $programme, ProgrammeRepository $programmeRepository): Response
+    {
+        $programme->setisValid(false);
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($programme);
+        $entityManager->flush();
+        $programmes = $programmeRepository->findBy([], ['id' => 'asc']);
+        $this->addFlash('success', 'vous venez de desactivé le programme : ' . $programme->getIntitule() . ' !');
+
         return $this->render('admin/programmes/index.html.twig', [
             'programmes' => $programmes,
         ]);
