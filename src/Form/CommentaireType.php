ec8e5d12e041c48e64e0ced4fd547c9b21c5a069
mohamed67210 <mohamed-bechri@outlook.fr>
2023-05-25 16:46:22 +0200

    limitation de nombre de caracteres de message , en utilisant constraints et length dans le formType commentaire

--- src/Form/CommentaireType.php
+++ src/Form/CommentaireType.php
@@ -8,13 +8,20 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class CommentaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('message', TextareaType::class, ['label'=>'Donnez votre avis :','attr' => ['class' => "form-label"]])
+            ->add('message', TextareaType::class, ['label'=>'Donnez votre avis :','attr' => ['class' => "form-label",'placeholder'=>'votre message...'],
+            'constraints'=>[
+                new Length([
+                    'max'=>20,
+                    'maxMessage'=>'le message ne doit pas depasser {{ limit }} caractères'
+                ])
+            ]])
             // ->add('create_at')
             // ->add('user')
             // ->add('programme')
