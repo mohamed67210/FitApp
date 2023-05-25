ca60779d0e88eef7c484d6f6f7523eabae67778e
mohamed67210 <mohamed-bechri@outlook.fr>
2023-05-25 11:34:37 +0200

    home page :utilisation de glider js pour mettre en place un slide 'nouveauté' + css modification

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -141,4 +141,16 @@ if (deleteLinks != null) {
         });
     });
 }
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide',{
+        perPage : 3,
+        breakpoints :{
+            640 :{
+                perPage :1,
+            },
+        },
+    } );
+    splide.mount();
+  } );
+  
 
