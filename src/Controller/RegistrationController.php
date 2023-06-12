01a9f03257354dc8f7e5e538966d8130485fabc9
mohamed67210 <mohamed-bechri@outlook.fr>
2023-06-12 09:05:51 +0200

    pouvoir initialiser la photo de profil (supprimer photo et remettre la photo par default )

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -83,7 +83,7 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('message', 'Votre message est bien verifié.');
 
         return $this->redirectToRoute('app_home');
     }
