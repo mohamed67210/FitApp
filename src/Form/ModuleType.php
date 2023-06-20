2943e19e9b36e4c1bf42d5a998f57352731ecabf
mohamed67210 <mohamed-bechri@outlook.fr>
2023-06-20 09:18:52 +0200

    mettre en place des constraints pour les entrées intitule et description pour ajout module

--- src/Form/ModuleType.php
+++ src/Form/ModuleType.php
@@ -11,17 +11,36 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ModuleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('intitule',TextType::class,['required' => true,'attr'=>['class' => 'form-control']])
-            ->add('description',TextareaType::class,['required' => true,'attr'=>['class' => 'form-control']])
+            ->add('intitule',TextType::class,[
+                'required' => true,
+                'attr'=>['class' => 'form-control'],
+                'constraints' => [
+                    new Length([
+                        'max' => 50,
+                        'maxMessage' => "L'intitulé ne doit pas dépasser 50 caractères."
+                    ])
+                ]
+                ])
+            ->add('description',TextareaType::class,[
+                'required' => true,
+                'attr'=>['class' => 'form-control'],
+                'constraints' => [
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => "La description ne doit pas dépasser 255 caractères."
+                    ])
+                ]
+                ])
             ->add('video', FileType::class, ['required' => true, 'constraints' => [
                 new File([
-                    // 'maxSize' => '50000k',
+                    'maxSize' => '50000k',
                     'mimeTypes' => [
                         'video/mp4',
                     ],
